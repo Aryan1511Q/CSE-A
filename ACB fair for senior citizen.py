2a3e82a7041d180fa3576b6f4598c1f51b9d2c3c
Aryan kushwaha <130817731+aryankushwaha007@users.noreply.github.com>
2023-06-07 12:09:27 +0530

    Add files via upload

---
+++ ACB fair for senior citizen.py
@@ -0,0 +1,7 @@
+a=eval(input("Enter your date of year\n"))
+c=2023-a
+d=1020-((1020/100)*20)
+if c>60:
+    print("You are above 60 so the fair is",d)
+else:
+    print("You are below 60 so the fair is 1020")
