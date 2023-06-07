9e80b69215206d0c16c651f21d1bbfca970064ac
Aryan kushwaha <130817731+aryankushwaha007@users.noreply.github.com>
2023-06-07 12:22:27 +0530

    Add files via upload

---
+++ Blood donation rotract club.py
@@ -0,0 +1,8 @@
+a=input("Enter your name\n")
+b=int(input("enter your age\n"))
+c=eval(input("Enter your weight\n"))
+if (b>18 and c>40):
+    print("Criteria matched you are eligible for boold donation")
+else:
+    print("You are not eligible for blood donation")
+   
